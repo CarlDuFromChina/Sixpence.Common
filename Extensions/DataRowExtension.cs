d192585bd6a76141ab18e9a415883542373f38db
CarlDuFromChina <18556906294@163.com>
2022-01-27 16:49:22 +0800

    DataRow add ToDictionary extension method

--- Extensions/DataRowExtension.cs
+++ Extensions/DataRowExtension.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using System.Text;
 
 namespace Sixpence.Common
 {
     public static class DataRowExtension
     {
+        public static Dictionary<string, object> ToDictionary(this DataRow row)
+        {
+            var dict = row.Table.Columns
+              .Cast<DataColumn>()
+              .ToDictionary(c => c.ColumnName, c => row[c]);
+            return dict;
+        }
+
         public static Dictionary<string, object> ToDictionary(this DataRow dataRow, DataColumnCollection columnCollection)
         {
             var columns = new List<string>();
